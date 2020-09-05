0b87f7a9108b966738cc3cf9def8ed42e8319bf3
iwkt <iwk@mac.com>
2020-09-05 21:41:14 +0900

    202009052140

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -2,15 +2,33 @@ window.addEventListener('scroll',function () {
   let scroll = document.documentElement.scrollTop;
   document.querySelector('#scroll').innerHTML = scroll;
 
-  let slideElm = document.querySelectorAll('.slide_anime');
+  let slideElm = document.querySelectorAll('.slide_in');
   
   if (scroll > 200) {
-    for (let i = 0;  i < slideElm.length; i++) {
-      slideElm[i].classList.add('active');
-    } 
+    // ↓　つまずいた部分 for in はダメだった。
+    // for (let i in slideElm) {
+    //   slideElm[i].classList.add('active');
+    // }
+
+    // ↓のforはOK 
+    // for (let i = 0;  i < slideElm.length; i++) {
+    //   slideElm[i].classList.add('active');
+    // } 
+
+    // 結果的にforEachで簡素化
+    slideElm.forEach(value => value.classList.add('active'));
   } else {
-    for (let i = 0; i < slideElm.length; i++) {
-      slideElm[i].classList.remove('active');  
-    }
+    // for (let i = 0; i < slideElm.length; i++) {
+    //   slideElm[i].classList.remove('active');  
+    // }
+    slideElm.forEach(value => value.classList.remove('active'));
   } 
 })
+
+// const elem = document.querySelector('.slide_in');
+// const cb = function () {
+//   alert('just now');
+// }
+
+// const io = new IntersectionObserver(cb);
+// io.observe(elem);
