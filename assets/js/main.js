86bdabd15af4c680e2471a550a1cf15307dac29d
iwkt <iwk@mac.com>
2020-09-09 06:40:45 +0900

    202009090640

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,24 @@
-const child = document.querySelector('.child');
-const cb = function(entries, observer) {
-    entries.forEach(entry => {
-        if(entry.isIntersecting) {
-            entry.target.classList.add('active');
-            // observer.unobserve(entry.target);
-        } else {
-            entry.target.classList.remove('active');
-        }
-    });
-    // alert('intersecting');
-}
 const options = {
     root: null,
-    rootMargin: "-300px 0px",
-    threshold: [0, 0.5, 1]
+    rootMargin: '-30% 0px',
+    threshold: 0
 };
-const io = new IntersectionObserver(cb, options);
-io.observe(child);
+
+const obs = new IntersectionObserver(showIntersect, options);
+
+const slideIn = document.querySelectorAll('.slide_in');
+slideIn.forEach(sa => obs.observe(sa));
+
+
+function showIntersect(changes,observer) {
+    changes.forEach(change => {
+        if (change.isIntersecting) {
+            change.target.classList.add('active');
+            observer.unobserve(change.target);
+        } else {
+            change.target.classList.remove('active');
+        }
+    });
+}
+
+
